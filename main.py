eadec337ef9c8a73afe312a5340b6bcfbb635e45
maLeha <maLehaNP@users.noreply.github.com>
2024-06-17 09:46:16 +0400

    Finished qsort

--- main.py
+++ main.py
@@ -10,6 +10,21 @@ list_qs = [234, 23434234, 234234, 12341234, 1234, 123443, 6345756, 876, 2357, 46
 list_hs = [4, 1, 3, 2, 16, 9, 10, 14, 8, 7]
 
 
+def recursive_digit_sum(digit):
+    # print(f"recursive_digit_sum({digit})")
+    lenn = len(str(digit))
+    if lenn == 2:
+        # print(f"# if lenn == 2: recursive_digit_sum({int(str(digit)[0])}) + ({int(str(digit)[-1])})")
+        return recursive_digit_sum(int(str(digit)[0])) + (int(str(digit)[-1]))
+    elif lenn == 1:
+        # print(f"# elif lenn == 1: {str(digit)[0]}")
+        return int(str(digit)[0])
+    else:
+        # print(f"# else: recursive_digit_sum({digit // 10}) + ({int(str(digit)[-1])})")
+        return recursive_digit_sum(digit // 10) + (int(str(digit)[-1]))
+
+
+# Быстрая сортировка
 def quicksort(i: int, j: int):
     # print(f"# quicksort({i}, {j})")
     pivotindex = findpivot(i, j)
@@ -26,9 +41,9 @@ def quicksort(i: int, j: int):
 def findpivot(i: int, j: int):
     firstkey = list_qs[i]
     for k in range(i + 1, j):
-        if list_qs[k] > firstkey:
+        if recursive_digit_sum(list_qs[k]) > recursive_digit_sum(firstkey):
             return k
-        elif list_qs[k] < firstkey:
+        elif recursive_digit_sum(list_qs[k]) < recursive_digit_sum(firstkey):
             return i
     return -1
 
@@ -37,9 +52,9 @@ def partition(i: int, j: int, pivot: int):
     left = i
     right = j
     while left <= right:
-        while list_qs[left] < pivot:
+        while recursive_digit_sum(list_qs[left]) >= recursive_digit_sum(pivot):
             left += 1
-        while list_qs[right] >= pivot:
+        while recursive_digit_sum(list_qs[right]) < recursive_digit_sum(pivot):
             right -= 1
         if left < right:
             temp = list_qs[right]
@@ -48,6 +63,7 @@ def partition(i: int, j: int, pivot: int):
     return left
 
 
+# Пирамидальная сортировка
 def heapsort():
     n = len(list_hs) - 1
     for i in range(floor(n / 2), 1, -1):
